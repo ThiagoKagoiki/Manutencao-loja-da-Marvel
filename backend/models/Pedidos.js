55db4e089b69f0dd836768f2b00c4ed9a158694a
THIAGO YUKIO DA COSTA KAGOIKI <thiago_kagoiki@estudante.sesisenai.org.br>
2025-08-14 14:02:10 -0300

    Table Pedidos ajustada

--- backend/models/Pedidos.js
+++ backend/models/Pedidos.js
@@ -3,16 +3,24 @@ import { DataTypes } from 'sequelize';
 export default (sequelize) => {
   const Pedido = sequelize.define('Pedidos', {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      data_pedido: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-      }
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    data_pedido: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    qtd:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    valorTotal:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   });
 
-  return Pedido;
+return Pedido;
 };
