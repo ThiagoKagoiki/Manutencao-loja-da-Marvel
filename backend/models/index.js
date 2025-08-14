8ad55e6bd630c20972e73c90120601c20b0bb05d
THIAGO YUKIO DA COSTA KAGOIKI <thiago_kagoiki@estudante.sesisenai.org.br>
2025-08-14 14:33:53 -0300

    indo para o react

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import UserModel from './User.js';
 import ProdutoModel from './Produtos.js';
 import PedidoModel from './Pedidos.js';
-import ItensModel from './ItensPedido.js';
 
 dotenv.config();
 
@@ -22,17 +21,11 @@ const db = {
   sequelize,
   User: UserModel(sequelize),
   Produto: ProdutoModel(sequelize),
-  Pedido: PedidoModel(sequelize),
-  ItensPedido: ItensModel(sequelize)
+  Pedido: PedidoModel(sequelize)
 };
 
 db.User.hasMany(db.Pedido, { foreignKey: 'user_id' });
 db.Pedido.belongsTo(db.User, { foreignKey: 'user_id' });
 
-db.Pedido.hasMany(db.ItensPedido, { foreignKey: 'pedido_id' });
-db.ItensPedido.belongsTo(db.Pedido, { foreignKey: 'pedido_id' });
-
-db.Produto.hasMany(db.ItensPedido, { foreignKey: 'produto_id' });
-db.ItensPedido.belongsTo(db.Produto, { foreignKey: 'produto_id' });
 
 export default db;
---
+++ services/cadastro.js
@@ -0,0 +1,7 @@
+let nome
+let sobrenome
+let login
+let senha
+let cpf
+let telefone
+let endereco
